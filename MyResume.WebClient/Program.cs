cd9f425e84f41a2b00ce164133924960f7790779
aniqoff-DEV <andrey.emailforwork.dev@gmail.com>
2024-04-28 19:02:44 +0300

    add resume page

--- MyResume.WebClient/Program.cs
+++ MyResume.WebClient/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
+using MyResume.WebClient.Application.Contracts;
+using MyResume.WebClient.Application.Services;
 
 namespace MyResume.WebClient
 {
@@ -13,6 +15,7 @@ namespace MyResume.WebClient
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<IJobSeekerService, JobSeekerService>();
             builder.Services.AddFluentUIComponents();
 
             await builder.Build().RunAsync();
