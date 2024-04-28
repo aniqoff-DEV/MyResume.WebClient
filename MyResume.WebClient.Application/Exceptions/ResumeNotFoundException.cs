509823f80bdc2292f23d180939d36d18d038a4e7
aniqoff-DEV <andrey.emailforwork.dev@gmail.com>
2024-04-28 19:03:05 +0300

    error handler

---
+++ MyResume.WebClient.Application/Exceptions/ResumeNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using MyResume.WebClient.Application.Exceptions.Base;
+
+namespace MyResume.WebClient.Application.Exceptions
+{
+    public class ResumeNotFoundException : NotFoundException
+    {
+        public ResumeNotFoundException()
+        : base($"Это резюме не найдено..")
+        {
+
+        }
+    }
+}
