c5ac0c40e20b882d8a44d038fe6804713fc957f6
aniqoff-DEV <andrey.emailforwork.dev@gmail.com>
2024-05-01 20:04:46 +0300

    сorrect display of resume with file and photo has been made

--- MyResume.WebClient.Application/Responses/JobSeekerResponses/InfoOnCardJobSeekerResponse.cs
+++ MyResume.WebClient.Application/Responses/JobSeekerResponses/InfoOnCardJobSeekerResponse.cs
@@ -1,4 +1,11 @@
 ﻿namespace MyResume.WebClient.Application.Responses.JobSeekerResponses
 {
-    public record InfoOnCardJobSeekerResponse(Guid Id, string FullName, string Description, string? BranchName, string? CityName, int? DesiredSalary);
+    public record InfoOnCardJobSeekerResponse(
+        Guid Id,
+        string FullName, 
+        string Description,
+        byte[]? Avatar,
+        string? BranchName,
+        string? CityName,
+        int? DesiredSalary);
 }
